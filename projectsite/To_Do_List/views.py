b58a0793a0a206c1a26358779a8123b4c4869384
John Brence S. Condesa <144252928+Bitr3e@users.noreply.github.com>
2025-10-13 22:04:19 +0800

    Added Bootstrap

--- projectsite/To_Do_List/views.py
+++ projectsite/To_Do_List/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from To_Do_List.models import Task
 
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Task
+    context_object_name = 'home'
+    template_name = 'home.html'
+    paginate_by = 5
+    
