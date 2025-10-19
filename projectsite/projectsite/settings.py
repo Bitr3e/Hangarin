0280dcabe25cc598c5e18155fe73475899fe40e0
John Brence S. Condesa <144252928+Bitr3e@users.noreply.github.com>
2025-10-19 14:00:02 +0800

    change app name

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -80,8 +80,8 @@ ROOT_URLCONF = 'projectsite.urls'
 import os
 
 # --- Progressive Web App Settings --- 
-PWA_APP_NAME = 'ProjectSite' 
-PWA_APP_DESCRIPTION = "A Progressive Web App version of ProjectSite" 
+PWA_APP_NAME = 'Hangarin Task-Manager' 
+PWA_APP_DESCRIPTION = "A Progressive Web App version of Hangarin" 
 PWA_APP_THEME_COLOR = '#0A0A0A' 
 PWA_APP_BACKGROUND_COLOR = '#FFFFFF' 
 PWA_APP_DISPLAY = 'standalone' 
